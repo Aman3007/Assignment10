d98b4e6f9efdb2be388854b52cfb0cab05288629
Aman <amanchauhan30072002@gmail.com>
2025-07-19 02:09:26 -0500

    Update Note.jsx

--- src/Note.jsx
+++ src/Note.jsx
@@ -5,16 +5,16 @@ const Note = ({ title, content, id, ondelete }) => {
   };
 
   return (
-    <div className="w-[90%] sm:w-3/5 md:w-2/5 lg:w-2/7  rounded-xl border border-black shadow-md mt-4 mb-2 p-4 ml-2">
-      <h2 className="text-xl font-semibold">{title}</h2>
-      <p className="my-2">{content}</p>
-      <button
-        onClick={deleteNote}
-        className="bg-red-500 text-white font-bold px-4 py-2 rounded shadow hover:bg-red-600 transition-all duration-300 mt-2"
-      >
-        Delete
-      </button>
-    </div>
+   <div className="w-[90%] sm:w-3/5 md:w-2/5 lg:w-1/4 rounded-2xl border border-gray-200 shadow-xl shadow-emerald-300 bg-white p-6 m-4 transition-transform hover:scale-[1.02]">
+  <h2 className="text-2xl font-bold text-emerald-600 mb-2 tracking-wide">{title}</h2>
+  <p className="text-blue-700 mb-4">{content}</p>
+  <button
+    onClick={deleteNote}
+    className="bg-red-500 text-white font-semibold px-4 py-2 rounded-lg shadow-md hover:bg-red-600 transition-all duration-300"
+  >
+    Delete
+  </button>
+</div>
   );
 };
 
