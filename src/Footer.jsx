71742d8b7154dc350cc23a3fa05a72c5f01df9ef
Aman <amanchauhan30072002@gmail.com>
2025-07-26 02:20:17 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,13 @@
 
 
+
 const Footer = () => {
   return (
-    <div className="text-center bg-gray-800 text-white py-4 mt-auto">
+    <div className="text-center bg-gray-800 text-white py-4 mt-auto" >
       <p>&copy; 2025</p>
+        <p className="hover:text-amber-600 animate-slide" >Built by <a href="https://wondrous-cascaron-7a7d69.netlify.app/" target="_blank">Aman Chauhan • Save Notes • Stay Organized • Edit Notes • Create Notes</a></p>
     </div>
-  );
+  ); 
 };
 export default Footer;
+
