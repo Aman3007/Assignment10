ae60a8d8f213e2a921969fd41c7907f4bdfd94a0
Aman <amanchauhan30072002@gmail.com>
2025-07-26 02:20:37 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,8 @@
 
 const Header = () => {
   return (
-    <div className="w-full bg-orange-500 py-4 px-4 text-white font-extrabold text-2xl shadow">
+    <div className="w-full bg-orange-500 py-4 px-4 text-white font-extrabold text-2xl shadow flex gap-3">
+        <img src="/notepad-icon.png"  alt="Keeper Logo" className="w-7.5" />
       <h1>Keeper</h1>
     </div>
   );
