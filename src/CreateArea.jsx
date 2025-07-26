52dfef213267b43ae2ffad8a47f81329476fc3de
Aman <amanchauhan30072002@gmail.com>
2025-07-26 02:19:51 -0500

    Update CreateArea.jsx

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./index.css"
 import Sdata from "./Notes";
 
 
 
-
-const CreateArea = ({ onAdd }) => {
+const CreateArea = ({  onAdd, onEdit, editData, editingId, clearEdit  }) => {
   const [note, setNote] = useState({ title: "", content: "" });
 
+  useEffect(() => {
+    if (editingId !== null) {
+      setNote(editData);
+    }
+  }, [editingId, editData]);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setNote((prev) => ({ ...prev, [name]: value }));
@@ -19,7 +23,13 @@ const CreateArea = ({ onAdd }) => {
       alert("Please fill in all fields before submitting.");
       return;
     }
-    onAdd(note);
+    
+    if (editingId !== null) {
+      onEdit(editingId, note);
+      clearEdit();
+    } else {
+      onAdd(note);
+    }
     setNote({ title: "", content: "" });
   };
 
@@ -30,6 +40,7 @@ const CreateArea = ({ onAdd }) => {
         className="w-full max-w-md flex flex-col p-6 bg-black/40 text-black rounded-xl shadow-lg shadow-gray-500 my-6"
       >
         <input
+        id="title"
           type="text"
           placeholder="Title"
           name="title"
@@ -38,6 +49,7 @@ const CreateArea = ({ onAdd }) => {
           className="text-lg p-2 mb-4 rounded"
         />
         <textarea
+        id="content"
           placeholder="Content"
           name="content"
           value={note.content}
@@ -48,7 +60,7 @@ const CreateArea = ({ onAdd }) => {
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 font-bold rounded hover:bg-blue-600 transition"
         >
-          Add
+          {editingId !== null ? "Update Note" : "Add Note"}
         </button>
       </form>
     </div>
