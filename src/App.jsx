3a4da4608d2872b6462569f06589304f218ca99b
Aman <amanchauhan30072002@gmail.com>
2025-07-26 02:19:22 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,37 @@ import { useState } from 'react';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+const [editData, setEditData] = useState({ title: '', content: '' });
 
   const addNote = (note) => {
     setNotes((prev) => [...prev, note]);
   };
-
+const editNote = (id, updatedNote) => {
+  setNotes((prevNotes) =>
+    prevNotes.map((note, index) =>
+      index === id ? updatedNote : note
+    )
+  );
+};
   const deleteNote = (id) => {
     setNotes((prev) => prev.filter((_, i) => i !== id));
   };
 
+ const editnote = (id) => {
+  setEditingId(id);
+  setEditData(notes[id]);
+};
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <CreateArea onAdd={addNote} />
+     <CreateArea
+  onAdd={addNote}
+  onEdit={editNote}
+  editData={editData}
+  editingId={editingId}
+  clearEdit={() => setEditingId(null)} />
       <div className="flex flex-wrap gap-4.5">
         {notes.map((val, i) => (
           <Note
@@ -29,6 +47,7 @@ const App = () => {
             title={val.title}
             content={val.content}
             ondelete={deleteNote}
+            editdata={editnote}
           />
         ))}
       </div>
